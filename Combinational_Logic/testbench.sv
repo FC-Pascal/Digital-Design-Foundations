90d92288264481ee00493dde221c6cc1a2feeed6
FC-Pascal <107795111+FC-Pascal@users.noreply.github.com>
2026-09-24 16:01:20 +0100

    Add testbench for sillyfunction module

---
+++ Combinational_Logic/testbench.sv
@@ -0,0 +1,17 @@
+module testbench;
+    reg a, b, c; 
+    wire y;      
+
+    sillyfunction dut(.a(a), .b(b), .c(c), .y(y));
+
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(0, testbench);
+
+        a = 0; b = 0; c = 0; #10;
+        a = 1; b = 0; c = 0; #10;
+        a = 1; b = 1; c = 1; #10;
+
+        $finish;
+    end
+endmodule
